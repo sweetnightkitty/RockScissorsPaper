3188cd713432aa7b37f2c69f59d9d7e8ff6c19b8
SaraToth <saratoth101@gmail.com>
2024-04-02 18:14:06 +0900

    Convert from one round to 5 round game with function playGame. Adjusted getResultMessage getWinner and playRound functions to work with newly implemented playGame function. Update ReadMe with project information

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RockScissorsPaper
+This is a project assigned via the odin project. For information about the project and directions visit: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
+
+Game is designed to prompt the user to provide an answer (rock, scissors, or paper) 5 times in a row and then displays the results of each round, and final result in the console.
+
+Currently, if the user inputs an answer other than rock scissors or paper (case-insensitive) an alert will display advising user to refresh the page and start over.
--- javascript.js
+++ javascript.js
@@ -1,24 +1,4 @@
-console.log("Hello World")
 
-
-
-// run RPS between the two choices and return winning announcement
-    //function parameters - playerChoice, computerChoice
-    
-    //if player Choice === computerChoice display a draw
-    //if playerChoice is rock and computerChoice is scissors
-    //or playerChoice is paper and computerChoice is rock
-    //or playerChoice is scissors and computerChoice is paper display winning message
-    // else display losing message
-
-
-    //display message "You lose, rock beats paper"
-    //playerChoice should convert to lowercase
-
-
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
-playRound(playerChoice, computerChoice);
 
 function getPlayerChoice() {
     let playerInput = prompt("Choose rock, scissors, or paper").toLowerCase();
@@ -52,7 +32,7 @@ function getRandomNumber() {
 function playRound(playerChoice, computerChoice) {
     const winner = checkWinner(playerChoice, computerChoice);
     const resultMessage = getResultMessage(playerChoice, computerChoice, winner);
-    console.log(resultMessage);
+    return winner;
 }
 
 function checkWinner(playerChoice, computerChoice){
@@ -62,31 +42,68 @@ function checkWinner(playerChoice, computerChoice){
         || playerChoice === "paper" && computerChoice === "rock"
         || playerChoice === "scissors" && computerChoice === "paper") {
             return "player";
-        } else {
+        } else if(playerChoice === "rock" && computerChoice === "paper"
+        || playerChoice === "paper" && computerChoice === "scissors"
+        || playerChoice === "scissors" && computerChoice === "rock") {
+            return "computer";
+        } else if(playerChoice > computerChoice) {
+            return "player";
+        } else if(computerChoice > playerChoice) {
             return "computer";
         }
 }
 
-function getResultMessage(playerChoice, computerChoice, winner) {
+function getResultMessage(winner) {
     if(winner === "draw") {
         return "It's a draw!";
     } else if(winner === "player") {
-        const gameRule = getGameRule(playerChoice);
-        return "You won! " + gameRule;
+        return "You won!";
     } else {
-        const gameRule = getGameRule(computerChoice);
-        return "You lose! " + gameRule;
+        return "You lose!";
     }
 }
 
 function getGameRule(winningChoice) {
     if(winningChoice === "rock") {
-        return "Rock beats scissors!";
+        return " Rock beats scissors!";
     } else if(winningChoice === "paper") {
-        return "Paper beats rock!";
+        return " Paper beats rock!";
+    } else if(winningChoice === "scissors"){
+        return " Scissors beats paper";
     } else {
-        return "Scissors beats paper";
+        return "";
     }
 }
 
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (i = 0; i < 5; i++) {
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        let winner = playRound(playerChoice, computerChoice);
+        
+        if(winner === "player") {
+            playerScore++;
+            console.log("Player: " + playerScore + " Computer: " + computerScore);
+            console.log(getResultMessage(winner) + getGameRule(playerChoice));
+        } else if(winner === "computer") {
+            computerScore++;
+            console.log("Player: " + playerScore + " Computer: " + computerScore);
+            console.log(getResultMessage(winner) + getGameRule(computerChoice));
+        } else {
+            console.log("Player: " + playerScore + " Computer: " + computerScore);
+            console.log(getResultMessage(winner));
+        }
+    }
+
+    const finalWinner = checkWinner(playerScore, computerScore);
+    const resultMessage = getResultMessage(finalWinner);
+    console.log(resultMessage);
+
+
+}
+
+playGame();
 
